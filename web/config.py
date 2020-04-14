593e3ce5c217336c83ad2ea80d18d9a5e61d110f
zhengquan <ze.feng@outlook.com>
2020-04-14 08:42:13 +0800

    modify

--- web/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-
-load_dotenv()
-
-ENV = os.getenv("FLASK_ENV") or "development"
-SECRET_KEY = os.getenv("SECRET_KEY") or "wongxy"
-DEBUG = os.getenv("DEBUG") or True
